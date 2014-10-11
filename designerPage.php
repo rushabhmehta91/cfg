badbd34f46861af99a058db54119dc2c133d6901
Rushabh Mehta <rushabhmehta91@hotmail.com>
2014-10-11 08:18:48 -0400

    uploading file designpage

--- designerPage.php
+++ designerPage.php
@@ -5,46 +5,29 @@
  * Date: 10/11/2014
  * Time: 4:55 AM
  */
+include('ajax/get3dDesignQueue.php');
+$html=table();
+
 ?>
-<div>
-<table id="designEngineers" class="display" cellspacing="0" width="100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
+<link rel="stylesheet" href="./js/DataTables-1.10.3/media/css/jquery.dataTables.min.css" type="text/css">
+<script src="./js/DataTables-1.10.3/media/js/jquery.dataTables.min.js"></script>
+<script src="./js/jquery-2.1.1.min.js"></script>
+<script src="./js/jquery-ui-1.11.1.custom/jquery-ui.js"></script>
 
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-
-</table>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <h2>Design Volunteers</h2>
+</head>
+<body>
+<div id="abc">
+    <?php echo $html['success']; ?>
     </div>
-<script>
-
-    $(document).ready(){
-        $("#designEngineers").dataTable({
-            "ajax": "ajax/objects.txt",
-            "columns": [
-                { "data": "name" },
-                { "data": "position" },
-                { "data": "office" },
-                { "data": "extn" },
-                { "data": "start_date" },
-                { "data": "salary" }
-            ]
-        });
-    }
-    </script>
+</body>
+</html>
+<div id="dailog" title="Basic dialog">
+    <input id="requestid" visible="false" value="">
+    <input id="location" visible="true" >
+    <input id="priority" visible="true" >
+    <input id="deadline" visible="true" >
+    <input id="Description" visible="true" >
+    </div>
