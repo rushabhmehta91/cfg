ad3ede697dced08c5a268e662635b599a28975dd
Rushabh Mehta <rushabhmehta91@hotmail.com>
2014-10-11 09:22:15 -0400

    uploading file designpage

--- designerPage.php
+++ designerPage.php
@@ -10,7 +10,7 @@ $html=table();
 
 ?>
 <link rel="stylesheet" href="./js/DataTables-1.10.3/media/css/jquery.dataTables.min.css" type="text/css">
-<script src="./js/DataTables-1.10.3/media/js/jquery.dataTables.min.js"></script>
+
 <script src="./js/jquery-2.1.1.min.js"></script>
 <script src="./js/jquery-ui-1.11.1.custom/jquery-ui.js"></script>
 
@@ -24,10 +24,41 @@ $html=table();
     </div>
 </body>
 </html>
-<div id="dailog" title="Basic dialog">
-    <input id="requestid" visible="false" value="">
-    <input id="location" visible="true" >
-    <input id="priority" visible="true" >
-    <input id="deadline" visible="true" >
-    <input id="Description" visible="true" >
+<div id="dailogbox" title="Design Request">
+    Description: <input id="Description" visible="true" disable><br>
+   Location: <input id="location" visible="true" disabled><br>
+    Priority: <input id="priority" visible="true" disabled><br>
+    category: <input id="category" visible="true" disabled><br>
+    Deadline: <input id="deadline" visible="true" disabled><br>
+
+    <input id="requestid" hidden="true" value="">
     </div>
+<script>
+
+        $( "#dailogbox" ).dialog({
+            autoOpen: false,
+            height: 300,
+            width: 350,
+            show: {
+                effect: "blind",
+                duration: 1000
+            },
+            hide: {
+                effect: "explode",
+                duration: 1000
+            }
+        });
+$(".opendailog").click(function(){
+    $printingId=$(this).attr("#data-id");
+    $description=$(this).attr("#data-desc");
+    $catagory=$(this).attr("#data-cat");
+    $location=$(this).attr("#data-loc");
+    $priority=$(this).attr("#data-prio");
+    $deadline=$(this).attr("#data-dead");
+
+
+    $("#dailogbox").data("Description",$description, ,$catagory,"location",$location,"priority",$priority,"deadline", $deadline).dialog( "open" );
+
+});
+
+</script>
